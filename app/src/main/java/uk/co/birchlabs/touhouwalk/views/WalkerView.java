35a6109f2f7ebfae4dc1a51146a411993df670f3
Alex Birch <vengeance.mx@gmail.com>
2016-10-08 23:02:07 +0100

    move reimu to drawable so that she can be optimized by build. switch to SurfaceView because it's apparently faster. draw spritesheet to screen!

--- app/src/main/java/uk/co/birchlabs/touhouwalk/views/WalkerView.java
+++ app/src/main/java/uk/co/birchlabs/touhouwalk/views/WalkerView.java
@@ -1,10 +1,19 @@
 package uk.co.birchlabs.touhouwalk.views;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -14,10 +23,11 @@ import uk.co.birchlabs.touhouwalk.R;
  * Created by birch on 08/10/2016.
  */
 
-public class WalkerView extends View {
+public class WalkerView extends SurfaceView {
     private Paint systemPaint;
-    private DisplayMetrics metrics;
-    private WindowManager wm;
+    private SurfaceHolder holder;
+
+    private Bitmap bmp;
 
     public WalkerView(Context c, AttributeSet attributeSet) {
         super(c, attributeSet);
@@ -25,20 +35,48 @@ public class WalkerView extends View {
     }
 
     public WalkerView(Context c) {
-        super(c, null, R.style.Walker);
+        super(c);
         init(c);
     }
 
     private void init(Context c) {
 
+        setZOrderOnTop(true);
+        holder = getHolder();
+        holder.setFormat(PixelFormat.TRANSPARENT);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+            }
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Canvas c = holder.lockCanvas(null);
+                onDraw(c);
+                holder.unlockCanvasAndPost(c);
+            }
+
+            @Override
+            public void surfaceChanged(
+                    SurfaceHolder holder,
+                    int format,
+                    int width,
+                    int height
+            ) {
+
+            }
+        });
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.reimu);
+
+
         systemPaint = new Paint();
-        systemPaint.setAlpha(75);
+//        systemPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+//        systemPaint.setColor(Color.TRANSPARENT);
+//        systemPaint.setAlpha(75);
 
-        metrics = new DisplayMetrics();
-
-        wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
-
-        updateDisplay();
+//        updateDisplay();
 
     }
 
@@ -55,12 +93,14 @@ public class WalkerView extends View {
 
 //        wm.getDefaultDisplay().getMetrics(metrics);
 
-        canvas.drawRect(
-                0,
-                0,
-                getWidth(),
-                getHeight(),
-                systemPaint
-        );
+//        canvas.drawRect(
+//                0,
+//                0,
+//                getWidth(),
+//                getHeight(),
+//                systemPaint
+//        );
+//        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bmp, 10, 10, systemPaint);
     }
 }
